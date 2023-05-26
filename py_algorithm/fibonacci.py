30117a3fe64d914ef10eb4426bc24f0dc604f511
kdeveloper9588 <kjlcodewww@gmail.com>
2023-05-26 23:31:13 +0900

    python algorithm fibonacci

---
+++ py_algorithm/fibonacci.py
@@ -0,0 +1,13 @@
+# Memoization 초기화
+d = [0] * 100
+
+
+def fibo(x):
+    # base case
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    # 점화식
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
